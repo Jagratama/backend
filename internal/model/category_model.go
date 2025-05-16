d373b63c23508d8157ccc14b80c78fefea5b66ea
Zaki Choiruddin <muhzakich@students.amikom.ac.id>
2025-05-16 21:58:59 +0700

    fix(categories): add type column

--- internal/model/category_model.go
+++ internal/model/category_model.go
@@ -5,6 +5,7 @@ import "time"
 type Category struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
 	Name      string    `json:"name" gorm:"size:255;not null"`
+	Type      string    `json:"type" gorm:"size:255"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
